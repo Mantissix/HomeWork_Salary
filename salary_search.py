4206758cbb4932855768b657e83a1f2d5f5fa447
Mantissix <437897595@qq.com>
2021-08-04 10:08:50 +0800

    第三次修改，调整工资发放及到账信息

--- salary_search.py
+++ salary_search.py
@@ -5,4 +5,4 @@ def search():
     if salary.money == 1000:
         print("工资未到账，请等待")
     else:
-        print(f"工资已到账{salary.money}元,请注意查收")
+        print(f"工资到账{salary.money}元,请注意查收")
--- salary_send.py
+++ salary_send.py
@@ -2,4 +2,5 @@ import salary
 
 
 def pay():
+    print(f"已发工资{salary.money}元")
     salary.money = salary.money + 1000
